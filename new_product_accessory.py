df5414cbe5688f9157d46cbeb20f577bc171afa1
lilyi <lilyli@qnap.com>
2017-08-10 18:41:41 +0800

    update

--- new_product_accessory.py
+++ new_product_accessory.py
@@ -75,7 +75,7 @@ try:
         cursor2.execute(sql2)   
 #        print(sql2)
         results = cursor2.fetchall()
-        sql3 = "SELECT `id` FROM `NEW_PRODUCT_ACCESSORY` WHERE `sku` = '{}' AND `image` = '{}'".format(each_product[1], each_product[2])
+        sql3 = "SELECT `id` FROM `new_product_accessory` WHERE `sku` = '{}' AND `image` = '{}'".format(each_product[1], each_product[2])
         cursor1.execute(sql3)
 #        print(sql3)
         accessory_id = cursor1.fetchall()        
@@ -113,44 +113,56 @@ try:
     db2 = MySQLdb.connect(host="localhost",user="root",passwd="root",db="open_cart", charset='utf8')
     cursor1 = db1.cursor()
     cursor2 = db2.cursor()
-    sql1 = "SELECT `id`, `sku` FROM `NEW_PRODUCT_ACCESSORY`"
+    sql1 = "SELECT `id`, `sku` FROM `new_product_accessory`"
     cursor1.execute(sql1)
     accessory = cursor1.fetchall()
     c = 0
+    c_each_cate = 0
+    CHECK = []
+    CHECK2 = []
     for each_access in accessory:
         c += 1
-        sql2 = "SELECT `product_id` FROM `PRODUCT` WHERE `model` = '{}'".format(each_access[1])
+        sql2 = "SELECT `product_id` FROM `product` WHERE `model` = '{}'".format(each_access[1]) # 用 access sku 找到對應 id
         print("HERE!1")
         cursor2.execute(sql2)
         product_id = cursor2.fetchall()
         print("HERE!2")
-        sql3 = "SELECT `category_id` FROM `PRODUCT_TO_CATEGORY` WHERE `product_id` = {}".format(product_id[0][0])
+        sql3 = "SELECT `category_id` FROM `product_to_category` WHERE `product_id` = {}".format(product_id[0][0]) # 用 product_id 找到對應的 category_id 通常不只一筆
         print("HERE!3")
         cursor2.execute(sql3)
-        category_id = cursor2.fetchall()
-        print("HERE!4")
-        for each_cate in category_id:
+        category_ids = cursor2.fetchall()
+#        print("len(category_ids)={}".format(category_ids))
+        # 用 category_ids 跑回圈
+        for each_cate in category_ids:
             if len(each_cate) == 0:
+                c_each_cate += 1
                 each_cate = 0
-                sql4 = "SELECT `name` FROM `CATEGORY_DESCRIPTION` WHERE `category_id` = {}".format(each_cate)
+                sql4 = "SELECT `name` FROM `category_description` WHERE `category_id` = {}".format(each_cate) # 用 category_id 找對應 name (此處為 NAS/NVR name)
             else:
-                sql4 = "SELECT `name` FROM `CATEGORY_DESCRIPTION` WHERE `category_id` = {}".format(each_cate[0])
+                sql4 = "SELECT `name` FROM `category_description` WHERE `category_id` = {}".format(each_cate[0])
             print("HERE!5")
             cursor2.execute(sql4)
-            NAS_name = cursor2.fetchall() # 會包含 NVR 和 XXX Series 等以下與官網 nas 的 temp_name 對不上的名稱
+            NAS_name = cursor2.fetchall() # 會包含 NVR 和 XXX Series 等以下與官網 nas 的 temp_name 對不上的名稱，多語，所以有好幾個
             print("HERE!6")
-            if len(NAS_name) == 0:
+            each_nas = NAS_name[0][0].strip().lower()
+            #
+            
+            #
+            if len(NAS_name) == 0: # 基本上不會進來這個條件下
+                CHECK.append(each_cate[0]) 
                 NAS_name = ''
-                sql5 = "SELECT `ItemID` FROM `PRODUCT_ITEMS` WHERE `temp_name` = '{}'".format(NAS_name)
-            elif NAS_name[0][0][-1] == ' ':
-                sql5 = "SELECT `ItemID` FROM `PRODUCT_ITEMS` WHERE `temp_name` = '{}'".format(NAS_name[0][0][:-1])
+                sql5 = "SELECT `ItemID` FROM `product_items` WHERE `temp_name` = '{}'".format(NAS_name)
+#            elif NAS_name[0][0][-1] == ' ':
+#                sql5 = "SELECT `ItemID` FROM `PRODUCT_ITEMS` WHERE `temp_name` = '{}'".format(NAS_name[0][0][:-1])
             else:
-                sql5 = "SELECT `ItemID` FROM `PRODUCT_ITEMS` WHERE `temp_name` = '{}'".format(NAS_name[0][0])
+                sql5 = "SELECT `ItemID` FROM `product_items` WHERE `temp_name` = '{}'".format(each_nas) # 用 NAS name 找機種 ID
             print(sql5)
             cursor1.execute(sql5)
             item_id = cursor1.fetchall()
+            #
             print(item_id)
             if len(item_id) == 0:
+                CHECK2.append(each_nas)
                 item_id = 0
                 print(item_id)
                 sql6 = "INSERT IGNORE INTO NEW_PRODUCT_ACCESSORY_RELATED(accessory_id, product_id,\
@@ -174,3 +186,71 @@ taken = round(tStop - tStart)
 print("Done!\n")
 print("Time taken: ", round(taken//60), "(m)", round(taken%60), "(s)")
 print(c)
+print("c_each_cate = {}".format(c_each_cate))
+print("CHECK = {}".format(CHECK))
+print("CHECK2 = {}".format(len(set(CHECK2))))
+
+VS = []
+TS = []
+for i in set(CHECK2):
+    l = i.split('-')[0]
+    if l == 'vs':
+        VS.append(i)
+    else:
+        TS.append(i)
+print(TS.sort())
+# 找出 65 個 0 來自何處
+# 針對每個 TS 去找所有的 accessory_name
+#db1 = MySQLdb.connect(host="localhost",user="root",passwd="root",db="yen_nas", charset='utf8')
+Accessory = []
+db2 = MySQLdb.connect(host="localhost",user="root",passwd="root",db="open_cart", charset='utf8')
+cursor2 = db2.cursor()
+for each_TS in TS:
+    
+    sql = "SELECT `category_id` FROM `category_description` WHERE `name` = '{}'".format(each_TS.upper())
+    cursor2.execute(sql)
+    resu = cursor2.fetchall()
+    Accessory.append(resu)
+db2.close()
+count=0
+for each in Accessory:
+    count += len(each)
+    if len(each) == 0:
+        print("E")
+# SELECT `category_id` FROM `category_description` WHERE `name` = 'TS-109 Series' => 10 筆
+# count=187  共197個 access (有重複)
+Hey = [] #category_id
+for each in Accessory:
+    if len(each) != 0:
+        Hey.append(each[0])
+    set(Hey)
+db2 = MySQLdb.connect(host="localhost",user="root",passwd="root",db="open_cart", charset='utf8')
+cursor2 = db2.cursor()
+All_access_ids = []
+for i in Hey:
+    sql = "SELECT `product_id` FROM `product_to_category` WHERE `category_id` = '{}'".format(i[0])
+    cursor2.execute(sql)
+    res = cursor2.fetchall()
+    for j in res:
+        All_access_ids.append(j[0])
+db2.close()
+
+
+db2 = MySQLdb.connect(host="localhost",user="root",passwd="root",db="open_cart", charset='utf8')
+cursor2 = db2.cursor()
+sql = "SELECT DISTINCT `category_id` FROM `product_to_category`"
+cursor2.execute(sql)
+product_items1 = cursor2.fetchall()
+db2.close()
+q = []
+p = []
+res = []
+for each in product_items2:
+#    l = each[0].strip()
+#    res.append(l)
+    q.append(each[0])
+
+for i in p:
+    for j in res:
+        if i != j:
+            print (i)
