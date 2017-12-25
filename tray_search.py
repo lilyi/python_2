59b06545d3f42515e05673d53f465fcf29522d5a
lilyi <lilyli@qnap.com>
2017-12-25 18:48:47 +0800

    add if else

--- tray_search.py
+++ tray_search.py
@@ -1,27 +1,35 @@
 import os, MySQLdb, time, datetime
 import csv
 import sys
-
 # file_path = os.getcwd()
-
-
-
 try:
     # db_yen = MySQLdb.connect(host="10.8.2.125", user="marketing_query", passwd="WStFfFDSrzzdEQFW", db="yen_nas", charset='utf8')
     # db_web_analytics = MySQLdb.connect(host="10.8.2.125", user="marketing_query", passwd="WStFfFDSrzzdEQFW", db="web_analytics", charset='utf8')
     db_web_analytics = MySQLdb.connect(host="localhost", user="root", passwd="root", db="web_analytics", charset='utf8')
     # cursor_yen = db_yen.cursor()
     cursor_web_analytics = db_web_analytics.cursor()
-    sql_newTable ='''CREATE TABLE IF NOT EXISTS `tray_table` (
+    sql_newTables = ['''CREATE TABLE IF NOT EXISTS `tray_table` (
     `part_no` varchar(100) NOT NULL,
       `sku` varchar(100) NOT NULL,
       `created_at` int(10) NOT NULL DEFAULT '0',
       `updated_at` int(10) NOT NULL DEFAULT '0',
       PRIMARY KEY (`part_no`)
-    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;''',
     '''
-    cursor_web_analytics.execute(sql_newTable)
-    # db_yen.commit()
+    CREATE TABLE IF NOT EXISTS `customer_table` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+      `name` varchar(100) NOT NULL,
+      `address` varchar(100) NOT NULL,
+      `part_no` varchar(100) NOT NULL,
+      `created_at` int(10) NOT NULL DEFAULT '0',
+      `updated_at` int(10) NOT NULL DEFAULT '0',
+      PRIMARY KEY (`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;''']
+
+    for i in sql_newTables:
+        cursor_web_analytics.execute(i)
+        db_web_analytics.commit()
+
     file_path = 'C:\\Users\\Lily\\Documents\\GitHub\\python_2'
     os.chdir(file_path)
 
@@ -35,11 +43,19 @@ try:
         for row in reader:
             # print(row)
             # break
-            sql_insert = "INSERT INTO tray_table(part_no, sku, created_at, updated_at)" \
-                         "VALUES ('{}', '{}', '{}', '{}')".format(
-                str(row[1]), str(row[4]), int(time.time()), int(time.time()))
-            cursor_web_analytics.execute(sql_insert)
-            db_web_analytics.commit()
+            sql_check = "SELECT * FROM `tray_table` WHERE `part_no` = '{}' AND `sku` = '{}'".format(str(row[1]), str(row[4]))
+            check = cursor_web_analytics.execute(sql_check)
+            if check == 1:
+                sql_update = "UPDATE `tray_table` SET `part_no` = '{a}', `sku` = '{b}', updated_at = '{c}' WHERE `part_no` = '{a}' AND `sku` = '{b}'" \
+                    .format(a=str(row[1]), b=str(row[4]), c=int(time.time()))
+                cursor_web_analytics.execute(sql_update)
+                db_web_analytics.commit()
+            else:
+                sql_insert = "INSERT INTO tray_table(part_no, sku, created_at, updated_at)" \
+                             "VALUES ('{}', '{}', '{}', '{}')".format(
+                    str(row[1]), str(row[4]), int(time.time()), int(time.time()))
+                cursor_web_analytics.execute(sql_insert)
+                db_web_analytics.commit()
     print("Table created")
     # sql = "SELECT `slug`, `lang_set`, `cid` FROM `knowledge_base`"
     # cursor_yen.execute(sql)
@@ -52,3 +68,10 @@ except MySQLdb.Error as e:
     db_web_analytics.rollback()
 # db_yen.close()
 db_web_analytics.close()
+
+def part_no_check(part_no):
+    sql_find = "SELECT * FROM `tray_table` WHERE `part_no` = '{}'".format(str(part_no))
+    check = cursor_web_analytics.execute(sql_find)
+    if check == 1:
+        cursor_web_analytics.execute(sql_find)
+        product = cursor_web_analytics.fetchall()
